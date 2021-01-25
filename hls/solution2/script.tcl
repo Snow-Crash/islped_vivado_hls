7e72567dd515fbb8aff9ac17312c262d59d4cd73
Snow-Crash <hfang02@syr.edu>
2021-01-25 12:09:31 -0500

    update

--- hls/solution2/script.tcl
+++ hls/solution2/script.tcl
@@ -5,15 +5,15 @@
 ############################################################
 open_project hls
 set_top neuron
-add_files neuron.h
 add_files neuron.cpp
-add_files -tb tb_weight.csv
-add_files -tb tb_potential.csv
-add_files -tb tb_outputspike.csv
-add_files -tb tb_inputspike.csv
-add_files -tb neuron_test.cpp
-add_files -tb CSVparser.hpp -cflags "-std=c++0x"
+add_files neuron.h
 add_files -tb CSVparser.cpp -cflags "-std=c++0x"
+add_files -tb CSVparser.hpp -cflags "-std=c++0x"
+add_files -tb neuron_test.cpp
+add_files -tb tb_inputspike.csv
+add_files -tb tb_outputspike.csv
+add_files -tb tb_potential.csv
+add_files -tb tb_weight.csv
 open_solution "solution2"
 set_part {xc7a100tcsg324-1}
 create_clock -period 100MHz -name default
